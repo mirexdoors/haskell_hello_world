f5705c54becca87d5cc7a2c9874d162b22528f09
mirexdoors <mirex.doors@mail.ru>
2020-12-27 23:50:36 +0700

    hoogle funcs & lists

--- Func_declaration.hs
+++ Func_declaration.hs
@@ -1,1 +1,7 @@
 lenVec3 x y z = sqrt (x ^ 2 + y ^ 2 + z ^ 2)
+
+discount :: Double -> Double -> Double -> Double
+discount limit proc sum = if sum >= limit then sum * (100 - proc) / 100 else sum
+
+standardDiscount :: Double -> Double
+standardDiscount = discount 1000 5
