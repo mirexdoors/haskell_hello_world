b4c52b6784b8cc3ce4ca1ada84dca2a11f873848
mirexdoors <mirex.doors@mail.ru>
2021-01-02 01:18:52 +0700

    local binding

--- Recursion.hs
+++ Recursion.hs
@@ -52,3 +52,12 @@ helper2 acc1 acc2 n  | n == 0 = acc1
 --fibonacciNegativeHelper acc3 0 = 0
 --fibonacciNegativeHelper acc3 (- 1) = 1
 --fibonacciNegativeHelper acc3 n = acc3 - fibonacciNegativeHelper (acc3 - n + 2) (n + 1)
+
+seqA :: Integer -> Integer
+seqA n | n == 0 = 1
+       | n == 1 = 2
+       | n == 2 = 3
+       | n > 2  =  let
+                     recurrently acc1 acc2 acc3 0 = acc1
+                     recurrently acc1 acc2 acc3 n = recurrently acc2 acc3 ((acc3 + acc2) - 2*acc1) (n-1)
+                   in recurrently 1 2 3 n
