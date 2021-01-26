48db3adf39af131e4b0af12fea9f4534e4938b42
mikhail <prokudin.mi@sibvaleo.com>
2021-01-26 16:01:30 +0700

    standart type classes ex2

--- Standart_Type_Classes.hs
+++ Standart_Type_Classes.hs
@@ -31,3 +31,14 @@ class (KnownToGork a, KnownToMork a) => KnownToGorkAndMork a where
     			  | doesEnrageGork x = stab x
     			  | doesEnrageMork x = stomp x
                   | otherwise = x
+
+
+--Имея функцию ip = show a ++ show b ++ show c ++ show d определите значения a, b, c, d так, чтобы добиться следующего поведения:
+--GHCi> ip
+--"127.224.120.12"
+
+a = read (show 127) :: Double
+b = read (show 224) :: Double
+c = read (show 120) :: Double
+d = read (show 12) :: Double
+ip = show a ++ show b ++ show c ++ show d
