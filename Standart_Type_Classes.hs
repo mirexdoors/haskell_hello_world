76a28b7e5ae1ba678aa7bf07bda4067a87e718a3
mikhail <prokudin.mi@sibvaleo.com>
2021-01-28 10:50:56 +0700

    standart type classes ex4

--- Standart_Type_Classes.hs
+++ Standart_Type_Classes.hs
@@ -68,3 +68,16 @@ class (Enum a, Bounded a, Eq a) => SafeEnum a where
           | otherwise = pred x
 
 instance SafeEnum Bool
+
+
+--Напишите функцию с сигнатурой:
+
+--avg :: Int -> Int -> Int -> Double
+--вычисляющую среднее значение переданных в нее аргументов:
+--
+--GHCi> avg 3 4 8
+--5.0
+
+avg :: Int -> Int -> Int -> Double
+avg a b c = fromInteger (toInteger a + toInteger b + toInteger c) / 3
+
