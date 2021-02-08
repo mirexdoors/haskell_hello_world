6fb158eed5396cdadcbd97219c1c3c39f9cab51c
mikhail <prokudin.mi@sibvaleo.com>
2021-02-08 10:12:43 +0700

    codewars ex

--- Codewars.hs
+++ Codewars.hs
@@ -116,3 +116,43 @@ toJadenCase [] = []
 toJadenCase js = unwords $ map capWord $ words js
 
 capWord word = [Char.toUpper $ head word] ++ (map Char.toLower $ tail word)
+
+----------------------------------------------------------------------------------------------------------------
+
+--In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+--
+--Example:
+--
+--highAndLow "1 2 3 4 5")  # return "5 1"
+--highAndLow "1 2 -3 4 5") # return "5 -3"
+--highAndLow "1 9 3 4 -5") # return "9 -5"
+--Notes:
+--
+--All numbers are valid Int32, no need to validate them.
+--There will always be at least one number in the input string.
+--Output string must be two numbers separated by a single space, and highest number is first.
+
+highAndLow :: String -> String
+highAndLow input =(show::Int->String) (maximum (getIntList input)) ++ " " ++ (show::Int->String) (minimum (getIntList input))
+
+getIntList :: [Char] -> [Int]
+getIntList input = map (read::String->Int) (words input)
+
+-----------------------------------------------------------------------------------------------------------
+--Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors
+--(except for 1 and the number itself), from smallest to largest.
+--If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+--Example:
+--divisors 12   -- should return Right [2,3,4,6]
+--divisors 25   -- should return Right [5]
+--divisors 13   -- should return Left "13 is prime"
+
+divisors :: (Show a, Integral a) => a -> Either String [a]
+divisors a | a < 2 = Left "arg must be > 1"
+           | a == 2 = Left "2 is prime"
+           | length (getDivisors a) == 0 = Left $ show a ++ " is prime"
+           | otherwise = Right $ getDivisors a
+
+
+getDivisors a = [x | x <- [2..a`div`2], a `rem` x == 0]
