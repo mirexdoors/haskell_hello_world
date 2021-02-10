ca028e85f1a54e55b9d5935704c4fd015bd4425e
mikhail <prokudin.mi@sibvaleo.com>
2021-02-10 09:41:50 +0700

    lists example

--- Lists.hs
+++ Lists.hs
@@ -1,1 +1,7 @@
 oddsOnly =  filter (odd)
+
+isPalindrom :: Eq a => [a] -> Bool
+isPalindrom list = list == reverse list where
+  reverse tmpList = rev tmpList [] where
+    rev [] b = b
+    rev (x:xs) b = rev xs (x:b)
