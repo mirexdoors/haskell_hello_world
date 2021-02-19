4f686c4fbcc4bd18714906357d08db79a6f010f0
mirexdoors <mirex.doors@mail.ru>
2021-02-20 02:06:46 +0700

    standart type classes ex

--- Lists.hs
+++ Lists.hs
@@ -57,3 +57,12 @@ qsort1 [] = []
 qsort1 [x] = [x]
 qsort1 (x:xs) = (qsort1 lessPart) ++ [x] ++ (qsort1 morePart) where
    (lessPart, morePart) = partition (<x) xs
+
+{-
+Напишите функцию squares'n'cubes, принимающую список чисел,
+и возвращающую список квадратов и кубов элементов исходного списка.
+GHCi> squares'n'cubes [3,4,5]
+[9,27,16,64,25,125]
+-}
+squares'n'cubes :: Num a => [a] -> [a]
+squares'n'cubes = concatMap (\x -> [x^2, x^3])
