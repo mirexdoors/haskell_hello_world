f14775dac5cb292ff4823f35ae23e97b69e22acb
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-03-23 16:57:33 +0700

    add stepik new exs

--- Lists.hs
+++ Lists.hs
@@ -238,3 +238,16 @@ lastElem = foldl1 undefined-}
 lastElem :: [a] -> a
 lastElem = foldl1 (flip const)
 
+-----------------------------------------------------
+{-
+Используя unfoldr, реализуйте функцию, которая возвращает в обратном алфавитном порядке список символов, попадающих в заданный парой диапазон.
+ Попадание символа x в диапазон пары (a,b) означает, что x >= a и x <= b.
+revRange :: (Char,Char) -> [Char]
+revRange = unfoldr g
+  where g = undefined
+GHCi> revRange ('a','z')
+"zyxwvutsrqponmlkjihgfedcba"-}
+
+evRange :: (Char,Char) -> [Char]
+evRange (a,b) = unfoldr (\x -> if x < a then Nothing else Just (x, pred x)) b
+
