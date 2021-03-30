8efa813e1808e71a15b8d5a69ae656cfe988691a
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-03-30 13:48:24 +0700

    add stepik new exs

--- Data_Types.hs
+++ Data_Types.hs
@@ -33,3 +33,41 @@ cmp Info Info = EQ
 cmp Error Error = EQ
 cmp Warning Warning = EQ
 
+-----------------------------------------------------
+{-
+Пусть объявлен следующий тип данных:
+
+data Result = Fail | Success
+
+
+И допустим определен некоторый тип данных SomeData и некоторая функция
+doSomeWork :: SomeData -> (Result,Int)
+возвращающая результат своей работы и либо код ошибки в случае неудачи, либо 0 в случае успеха.
+Определите функцию processData, которая вызывает doSomeWork и возвращает строку "Success" в случае ее успешного завершения,
+ либо строку "Fail: N" в случае неудачи, где N — код ошибки.-}
+
+data Result = Fail | Success
+
+data SomeData
+
+doSomeWork :: SomeData -> (Result,Int)
+doSomeWork = undefined
+
+processData :: SomeData -> String
+processData d = case doSomeWork d of
+                        (Success, _) -> "Success"
+                        (Fail, errNum) -> "Fail: " ++ show errNum
+
+
+------------------------------------------------------------
+
+data Point = Point Double Double
+
+origin :: Point
+origin = Point 0.0 0.0
+
+distanceToOrigin :: Point -> Double
+distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
+
+distance :: Point -> Point -> Double
+distance (Point a b) (Point x y) = sqrt ((a - x)**2 + (b - y)**2)
