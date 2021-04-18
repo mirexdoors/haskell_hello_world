b9e4b7e45d8da9a69d91510c09da77f9ca7745ef
mirexdoors <mirex.doors@mail.ru>
2021-04-18 18:29:37 +0700

    add data types exs from stepik

--- Data_Types.hs
+++ Data_Types.hs
@@ -79,8 +79,46 @@ data Shape = Circle Double | Rectangle Double Double
 У него два конструктора: Circle r — окружность радиуса r, и Rectangle a b — прямоугольник с размерами сторон a и b.
  Реализуйте функцию area, возвращающую площадь фигуры.-}
 
-data Shape = Circle Double | Rectangle Double Double deriving Show
+--data Shape = Circle Double | Rectangle Double Double deriving Show
+--
+--area :: Shape -> Double
+--area (Circle r) = pi * (r ** 2)
+--area (Rectangle a b) = a * b
 
-area :: Shape -> Double
-area (Circle r) = pi * (r ** 2)
-area (Rectangle a b) = a * b
+----------------------------------------------------------------
+
+{-
+Функция doSomeWork возвращала результат своей работы и либо код ошибки в случае неудачи, либо 0 в случае успеха.
+Такое определение функции не является наилучшим, так как в случае успеха мы вынуждены возвращать некоторое значение, которое не несет никакой смысловой нагрузки.
+
+Используя функцию doSomeWork, определите функцию doSomeWork' так, чтобы она возвращала код ошибки только в случае неудачи.
+ Для этого необходимо определить тип Result'.
+ Кроме того, определите instance Show для Result' так, чтобы show возвращал "Success" в случае успеха и "Fail: N" в случае неудачи, где N — код ошибки.-}
+
+--data Result = Fail | Success
+--data Result' = Result Int
+--
+--instance Show Result' where
+--    show (Result n) = case n of
+--                       0 -> "Success"
+--                       otherwise -> "Fail: " ++ show n
+--
+--doSomeWork' :: SomeData -> Result'
+--doSomeWork' someData = case doSomeWork someData of
+--                          (Success, 0)  -> (Result 0)
+--                          (Fail, n )  -> (Result n)
+
+----------------------------------------------------------------------------------------------
+
+--Реализуйте функцию isSquare, проверяющую является ли фигура квадратом.
+
+data Shape = Circle Double | Rectangle Double Double
+  deriving Show
+
+square :: Double -> Shape
+square a = Rectangle a a
+
+isSquare :: Shape -> Bool
+isSquare (Circle a) = False
+isSquare (Rectangle a b) | a == b = True
+						 | otherwise = False
