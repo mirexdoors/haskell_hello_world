3aa5a376bbaf993dbcf6a57d16d0a9acd3c72044
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-06-08 10:44:30 +0700

    add cata

--- Codewars.hs
+++ Codewars.hs
@@ -448,6 +448,36 @@ cleanString = reverse . foldl pos ""
   where pos cs '#' = drop 1 cs
         pos cs  c  = c : cs
 
+---------------------------------------------
+{-
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+-}
+
+yourOrderPlease :: String -> String
+yourOrderPlease = processTulpesToString .sortPairs. processWordsForPair . words
+
+processWordsForPair :: [String] -> [(Int, String)]
+processWordsForPair = map (\x -> (getIntCharFromString x, x))
+
+getIntCharFromString :: [Char] -> Int
+getIntCharFromString str = read (filter (\x -> Char.isDigit x) str) :: Int
+
+sortPairs ::  [(Int, String)] ->  [(Int, String)]
+sortPairs =  sortBy (\x y -> if (fst x > fst y ) then  GT else LT)
+
+processTulpesToString ::  [(Int, String)] -> String
+processTulpesToString =unwords . (map snd)
+
+-- 1. превратить строку в массив слов [x]
+-- 2. преврать массив в массив пар [x]
+-- 3. сортировать [x]
+-- 4 склеить [x]
 
