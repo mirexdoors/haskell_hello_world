ceef09e74c0bf9214554589b8a8e9d4d9d371333
mirexdoors <mirex.doors@mail.ru>
2021-07-13 02:06:22 +0700

    some kata with binary tree height and size from stepik

--- Recursive_Types.hs
+++ Recursive_Types.hs
@@ -56,3 +56,15 @@ data Tree a = Leaf a | Node (Tree a) (Tree a)
 Реализуйте функцию height, возвращающую высоту дерева, и функцию size,
  возвращающую количество узлов в дереве (и внутренних, и листьев). Считается, что дерево, состоящее из одного листа, имеет высоту 0.
  -}
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+height :: Tree a -> Int
+height (Leaf _) = 0
+height (Node a b) = 1 + max (height a) (height b)
+
+size :: Tree a -> Int
+size (Leaf _) = 1
+size (Node a b) = 1 + size a + size b
+
+exampleTree =  Node (Leaf 1 ) (Leaf 1)
