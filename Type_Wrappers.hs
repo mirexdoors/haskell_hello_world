db4b3f297c6fcc6f5ee9b75c2c74cf20cf084dda
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-08-10 00:54:41 +0700

    realize another monoid task

--- Type_Wrappers.hs
+++ Type_Wrappers.hs
@@ -11,3 +11,18 @@ instance Semigroup Xor where
 instance Monoid Xor where
   mempty = Xor False
   mappend = (Data.Semigroup.<>)
+
+----------------------------------------------------------
+{-
+Реализуйте представителя класса типов Monoid для Maybe' a так, чтобы mempty не был равен Maybe' Nothing.
+ Нельзя накладывать никаких дополнительных ограничений на тип a, кроме указанных в условии.-}
+
+newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
+    deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Maybe' a) where
+    Maybe' (Just x) <> Maybe' (Just y) = Maybe' (Just (x <> y))
+    _  <> _                            = Maybe' Nothing
+
+instance Monoid a => Monoid (Maybe' a) where
+    mempty = Maybe' (Just mempty)
