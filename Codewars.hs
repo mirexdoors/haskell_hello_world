834ed31de57b744b7612aeadd4a739a1c51b4452
Mikhail Prokudin <mirex.doors@mail.ru>
2021-09-28 13:30:29 +0700

    add kata

--- Codewars.hs
+++ Codewars.hs
@@ -490,3 +490,23 @@ There is a large pile of socks that must be paired by color.
 
 solve :: [Int] -> Int
 solve = sum . map (\xs -> length xs `div` 2 ) . group . sort
+
+---------------------------------------------------------------
+{-
+Given a triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+find the triangle's row knowing its index (the rows are 1-indexed), e.g.:
+
+odd_row(1)  ==  [1]
+odd_row(2)  ==  [3, 5]
+odd_row(3)  ==  [7, 9, 11]
+Note: your code should be optimized to handle big inputs. -}
+
+oddRow :: Integer -> [Integer]
+oddRow x = take (fromIntegral x) $ [y | y <- [(x*(x-1) + 1)..], odd y]
