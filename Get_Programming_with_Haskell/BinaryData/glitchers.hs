f5337287431c434368a62b963600c59c67078561
Mikhail Prokudin <mirex.doors@mail.ru>
2022-07-12 22:04:20 +0700

    draft: start glitcher

---
+++ Get_Programming_with_Haskell/BinaryData/glitchers.hs
@@ -0,0 +1,22 @@
+import System.Environment
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+    where safeInt = int `mod` 255
+
+
+intToBC :: Int -> BC.ByteString
+intToBC int = BC.pack [intToChar int]
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let fileName = head args
+    imageFile <- BC.readFile fileName
+    glitched <- return imageFile
+    let glitchedFileName = mconcat["glitched_", fileName]
+    BC.writeFile glitchedFileName glitched
+    putStrLn "Done!"
