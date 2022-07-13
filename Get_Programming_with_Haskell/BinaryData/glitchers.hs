a501dd2f0f0e8c600b7f78fd11a52b263aab2167
Mikhail Prokudin <mirex.doors@mail.ru>
2022-07-14 00:08:28 +0700

    finish jpg glitcher app

--- Get_Programming_with_Haskell/BinaryData/glitchers.hs
+++ Get_Programming_with_Haskell/BinaryData/glitchers.hs
@@ -1,4 +1,6 @@
 import System.Environment
+import System.Random
+import Control.Monad
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 
@@ -10,13 +12,40 @@ intToChar int = toEnum safeInt
 intToBC :: Int -> BC.ByteString
 intToBC int = BC.pack [intToChar int]
 
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+replaceByte loc chV bytes = mconcat [before, newChar, after]
+    where (before, rest) = BC.splitAt loc bytes
+          after = BC.drop 1 rest
+          newChar = intToBC chV
 
+randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte bytes = do
+    let bytesLength = BC.length bytes
+    location <- randomRIO (1, bytesLength)
+    chV <- randomRIO (0, 255)
+    return (replaceByte location chV bytes)
+
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before, changed, after]
+    where (before, rest) = BC.splitAt start bytes
+          (target, after) = BC.splitAt size rest
+          changed = BC.sort target
+
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+    let sectionSize = 25
+    let bytesLength = BC.length bytes
+    start <- randomRIO (0, bytesLength - sectionSize)
+    return (sortSection start sectionSize bytes)
+
+-- TODO: randomReverseByte:  reverse random bytes section
 main :: IO ()
 main = do
     args <- getArgs
     let fileName = head args
     imageFile <- BC.readFile fileName
-    glitched <- return imageFile
+    glitched <- foldM (\bytes func -> func bytes) imageFile
+                      [randomReplaceByte, randomSortSection, randomReplaceByte, randomSortSection, randomReplaceByte]
     let glitchedFileName = mconcat["glitched_", fileName]
     BC.writeFile glitchedFileName glitched
     putStrLn "Done!"
